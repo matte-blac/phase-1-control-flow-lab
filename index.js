56bce0b349dd8a5d5b52d2f540586763237428dd
Matthew Bomett <bomettmat2@gmail.com>
2023-09-19 00:11:50 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+const ride = 3000;
+const tip = "okay";
+let comment;
+
+function scuberGreetingForFeet(ride) {
+  if (ride <= 400) {
+    return 'This one is on me!';
+  } else if (ride > 2500) {
+    return 'No can do.';
+  } else if (ride > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      comment = "Thank you so much.";
+      break;
+    case 'not as generous':
+      comment = "Thank you.";
+      break;
+    default:
+      comment = "Bye.";
+      break;
+  }
+  return comment; // Return the comment
 }
